68513165b8f4d2c6743575dc016ede94790afa1f
Babyforce <babyforce@protonmail.com>
2025-02-03 09:15:21 +0100

    Added image pasting support and image display with Kitty only

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -17,7 +17,7 @@ return {
 		event = { "VeryLazy" },
 		build = "deno task --quiet build:fast",
 		config = function()
-		require("peek").setup()
+		require("peek").setup({app = { 'firefox', '--new-window' }})
 		vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
 		vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
 		end,
